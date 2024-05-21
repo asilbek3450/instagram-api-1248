a1dcdd5dd2e687c0d445d532e0f933d570ae69b8
asilbek <asilbekmirolimov@gmail.com>
2024-05-21 18:38:41 +0500

    changes

--- users/models.py
+++ users/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 from shared.models import BaseModel
 
-ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", 'manager', 'admin')
+ORDINARY_USER, MANAGER, ADMIN = ("ordinary_user", 'manager', 'admin')  # oddiy_user, manager, admin
 VIA_EMAIL, VIA_PHONE = ("via_email", "via_phone")
 NEW, CODE_VERIFIED, DONE, PHOTO_STEP = ('new', 'code_verified', 'done', 'photo_step')
 
@@ -29,21 +29,22 @@ class User(AbstractUser, BaseModel):
         (PHOTO_STEP, PHOTO_STEP)
     )
     user_roles = models.CharField(max_length=31, choices=USER_ROLES, default=ORDINARY_USER)
-    auth_type = models.CharField(max_length=31, choices=AUTH_TYPE_CHOICES)
+    auth_type = models.CharField(max_length=31, choices=AUTH_TYPE_CHOICES, default=VIA_PHONE)
     auth_status = models.CharField(max_length=31, choices=AUTH_STATUS, default=NEW)
     email = models.EmailField(null=True, blank=True, unique=True)
     phone_number = models.CharField(max_length=13, null=True, blank=True, unique=True)
-    photo = models.ImageField(upload_to='user_photos/', null=True, blank=True, validators=FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'heic', 'heif']))
+    photo = models.ImageField(upload_to='user_photos/', null=True, blank=True,
+                              validators=FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png', 'heic', 'heif']))
 
     def __str__(self):
         return self.username
 
     @property
-    def get_full_name(self):
+    def get_full_name(self):  # decorator
         return f"{self.first_name} {self.last_name}"
 
     def create_verify_code(self, verify_type):
-        code = "".join([str(random.randint(0, 100) % 10) for _ in range(4)])
+        code = "".join([str(random.randint(0, 100) % 10) for _ in range(4)])  # 6215
         UserConfirmation.objects.create(user_id=self.id, code=code, verify_type=verify_type)
         return code
 
@@ -60,13 +61,13 @@ class UserConfirmation(BaseModel):
     code = models.CharField(max_length=4)
     verify_type = models.CharField(max_length=31, choices=TYPE_CHOICES)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='verify_codes')
-    expiration_time = models.DateTimeField(null=True, blank=True)
+    expiration_time = models.DateTimeField(null=True, blank=True)  # yaroqlilik muddati
     is_confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.user.__str__())
 
-    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):  # args -> argument istalgancha, kwargs -> keyword argument
         if not self.pk:
             if self.verify_type == VIA_EMAIL:
                 self.expiration_time = datetime.now() + timedelta(minutes=EMAIL_EXPIRE)
